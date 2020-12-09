56fca25539d59a152ecaed0de3c3a4067e1ff9b4
vob <voblivion@hotmail.fr>
2020-12-08 21:17:10 -0500

    lib
    fix using STA for namespace not to collide with STD

--- include/vob/sta/library.h
+++ include/vob/sta/library.h
@@ -3,18 +3,18 @@
 #include <vob/sta/platform.h>
 
 #if defined(VOB_STA_WINDOWS)
-#	define VOB_STD_LIB_EXPORT __declspec(dllexport)
-#	define VOB_STD_LIB_IMPORT __declspec(dllimport)
-#	define VOB_STD_LIB_DEPRECATED __declspec(deprecated)
+#	define VOB_STA_LIB_EXPORT __declspec(dllexport)
+#	define VOB_STA_LIB_IMPORT __declspec(dllimport)
+#	define VOB_STA_LIB_DEPRECATED __declspec(deprecated)
 #	ifdef _MSC_VER
 #		pragma warning(disable: 4251)
 #	endif
 #elif __GNUC__ >= 4
-#	define VOB_STD_LIB_EXPORT __attribute__ ((__visibility__ ("default")))
-#	define VOB_STD_LIB_IMPORT __attribute__ ((__visibility__ ("default")))
-#	define VOB_STD_LIB_DEPRECATED __attribute__ ((deprecated))
+#	define VOB_STA_LIB_EXPORT __attribute__ ((__visibility__ ("default")))
+#	define VOB_STA_LIB_IMPORT __attribute__ ((__visibility__ ("default")))
+#	define VOB_STA_LIB_DEPRECATED __attribute__ ((deprecated))
 #else
-#	define VOB_STD_LIB_EXPORT
-#	define VOB_STD_LIB_IMPORT
-#	define VOB_STD_LIB_DEPRECATED __attribute__ ((deprecated))
+#	define VOB_STA_LIB_EXPORT
+#	define VOB_STA_LIB_IMPORT
+#	define VOB_STA_LIB_DEPRECATED __attribute__ ((deprecated))
 #endif
