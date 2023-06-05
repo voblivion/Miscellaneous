79f1e4f2901326914b36e326f5804bf3485f22be
vob <voblivion@hotmail.fr>
2023-06-05 12:05:25 -0700

    added utility to combine measure types

--- include/vob/misc/physics/measure_util.h
+++ include/vob/misc/physics/measure_util.h
@@ -7,10 +7,22 @@ namespace vob::misph
 {
 	namespace measure_util
 	{
+		template <typename TMeasure1, typename TMeasure2>
+		using multiply_t = decltype(std::declval<TMeasure1>() * std::declval<TMeasure2>());
+
+		template <typename TMeasure1, typename TMeasure2>
+		using divide_t = decltype(std::declval<TMeasure1>() / std::declval<TMeasure2>());
+
+		template <typename TMeasure>
+		using rate_t = decltype(std::declval<TMeasure>() / 1_s);
+
 		template <typename TUnit, typename TRepresentation>
 		constexpr auto sqrt(measure<TUnit, TRepresentation> const& a_measure)
 		{
 			return detail::measure_make<unit_sqrt_t<TUnit>>(std::sqrt(a_measure.value()));
 		}
+
+		template <typename TMeasure>
+		using sqrt_t = decltype(sqrt(std::declval<TMeasure>()));
 	}
 }
