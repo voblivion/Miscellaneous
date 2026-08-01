f88adf95e6daa7ad75717869f0811612f00d40f7
vob <voblivion@hotmail.fr>
2026-08-01 01:42:28 -0400

    [build] assess warnings

--- include/vob/misc/std/ignorable_assert.h
+++ include/vob/misc/std/ignorable_assert.h
@@ -2,4 +2,8 @@
 
 #include <vob/misc/std/debug_break.h>
 
+#if defined(_MSC_VER)
+#define ignorable_assert(cond) __pragma(warning(suppress: 4127)) if (!(cond)) debug_break();
+#else
 #define ignorable_assert(cond) if (!(cond)) debug_break();
+#endif
